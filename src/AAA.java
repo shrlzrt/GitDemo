d9fc7d8b9ae68d25d2edca25ea5cf73ef5e5727a
Sharlyn Z <sharlynzarate@gmail.com>
2024-01-24 14:47:32 +0800

    US timezone develop

---
+++ src/AAA.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class AAA {
+
+	@Test
+	public void TC1() {
+		System.out.println("GitDemo develop branch");	
+	}
+	@Test
+	public void TC2() {
+		System.out.println("GitDemo US timezone");	
+	}
+
+}
